dd31fd6b11a7751a3dee9b16b28d11162ba3a06d
Kozinets Svyatoslav <Kozinets.S.Vi@sberbank.ru>
2023-12-15 17:57:23 +0300

    db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,25 @@
 import { Module } from "@nestjs/common"
 import { UsersModule } from "./users/users.module"
-import { TasksModule } from './tasks/tasks.module';
+import { TasksModule } from "./tasks/tasks.module"
 import { CommentsModule } from "./comments/comments.module"
+import { SequelizeModule } from "@nestjs/sequelize"
 
 @Module( {
-    imports: [ UsersModule, TasksModule, CommentsModule ],
+    imports: [
+        SequelizeModule.forRoot( {
+            dialect: "postgres",
+            host: "localhost",
+            port: 5432,
+            username: "postgres",
+            password: "root",
+            database: "todolist",
+            models: [],
+            autoLoadModels: true
+        } ),
+        UsersModule, TasksModule, CommentsModule ],
     controllers: [],
     providers: []
 } )
+
 export class AppModule {
 }
